a1c2f4506be74a14da18ac3e49b59cb61d7c3039
Aymen MOUSSI <aymen.moussi.ext@mappy.com>
2018-07-30 17:17:56 +0200

    first actor release

--- build.sbt
+++ build.sbt
@@ -3,4 +3,8 @@ name := "scala-akka-shaping-up"
 version := "1.0"
 
 scalaVersion := "2.12.1"
-        
+
+libraryDependencies ++= Seq(
+    "com.typesafe.akka" %% "akka-actor" % "2.5.14",
+    "com.typesafe.akka" %% "akka-testkit" % "2.5.14" % Test
+)
---
+++ src/main/scala/akka/ActorCreation.scala
@@ -0,0 +1,25 @@
+package akka
+
+import akka.actor.{Actor, ActorSystem, Props}
+
+/**
+  * Created by amoussi on 30/07/18.
+  */
+
+class GreetingActor extends Actor {
+    def receive = {
+        case "hello" => println("helloo")
+        case _ => println("Not Recognized message")
+    }
+}
+
+object main extends App {
+
+    val system = ActorSystem("GreetingActor")
+
+    val greetingActor = system.actorOf(Props[GreetingActor], "greeting")
+
+    greetingActor ! "hello"
+    greetingActor ! "Salem"
+}
+
