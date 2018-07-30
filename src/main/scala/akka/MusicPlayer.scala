32033ac528b5cd22b5bc31947b2373efa883c139
Aymen MOUSSI <aymen.moussi.ext@mappy.com>
2018-07-30 17:47:50 +0200

    actor based on class types

--- src/main/scala/akka/MusicPlayer.scala
+++ src/main/scala/akka/MusicPlayer.scala
@@ -1,20 +1,19 @@
 package akka
 
 import akka.MusicController.{PlayMusicMsg, StopMusicMsg}
+import akka.MusicPlayer.{Play, Stop}
 import akka.actor.{Actor, ActorSystem, Props}
 
 /**
   * Created by amoussi on 30/07/18.
   */
-class MusicPlayer {
-
-}
 
 object MusicController {
 
-    case class PlayMusicMsg(who: String)
+    sealed trait MusicMsg
+    case class PlayMusicMsg(who: String) extends MusicMsg
 
-    case class StopMusicMsg(who: String)
+    case class StopMusicMsg(who: String) extends MusicMsg
 
     val props = Props[MusicController]
 }
@@ -24,17 +23,34 @@ class MusicController extends Actor {
     def receive = {
         case PlayMusicMsg(who) => println(s" play music for $who")
         case StopMusicMsg(who) => println(s" stop music for $who")
-        case _ => println("unrecognized music player action")
+        case _ => println("unrecognized music controller action")
     }
+}
+
+object MusicPlayer {
+    sealed trait PlayerMsg
+    case class Play(piste:String) extends PlayerMsg
+    case class Stop(piste:String) extends PlayerMsg
+    val props = Props[MusicPlayer]
+}
+
+class MusicPlayer extends Actor{
+ def receive = {
+     case Play(piste) => val musicControllerActor = context.actorOf(MusicController.props, "musicController")
+         musicControllerActor ! PlayMusicMsg(piste)
+     case Stop(piste) => val musicControllerActor = context.actorOf(MusicController.props, "musicController")
+         musicControllerActor ! StopMusicMsg(piste)
+     case _ => println("unrecognized message")
+ }
 }
 
 object  player extends App{
 
     val system = ActorSystem("musicPlayer")
-    val musicController = system.actorOf(MusicController.props, "musicController")
+    val musicPlayer = system.actorOf(MusicPlayer.props, "musicPlayer")
 
-    musicController ! PlayMusicMsg("Afassi Quran")
-    musicController ! StopMusicMsg("Ali Jaber Quran")
+    musicPlayer ! Play("Afassi Quran")
+    musicPlayer ! Stop("Ali Jaber Quran")
 
     system.terminate
 }
